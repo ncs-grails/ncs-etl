d31bca9e4b64b8e8fd355c6503675ccf0ef4ea1b
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-02-02 01:12:45 +0000

    upgrading shibboleth
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-etl@879 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
     plugins {
 		compile ":spring-security-core:1.2.7.2"
 		compile ":spring-security-ldap:1.0.5.1"
-		compile ":spring-security-shibboleth-native-sp:1.0.2"
+		compile ":spring-security-shibboleth-native-sp:1.0.3"
 		provided ":spring-security-mock:1.0.1"
 	}
 }
